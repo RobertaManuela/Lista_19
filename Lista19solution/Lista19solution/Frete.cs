0bb7234d0469a4ff01793e56a2291594135768a6
Roberta Manuela <robertamanuellyaraujo@hotmail.com>
2019-10-24 14:25:33 -0300

    ex02

---
+++ Lista19solution/Lista19solution/Frete.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista19solution
+{
+    class Frete
+    {
+        private double distancia, peso;
+
+        public Frete(double d, double p)
+        {
+            if (d > 0)
+            {
+                distancia = d;
+            }
+
+            if (p > 0)
+            {
+                peso = p;
+            }
+        }
+
+        public double GetPeso()
+        {
+            return peso;
+        }
+
+        public double GetDistancia()
+        {
+            return distancia;
+        }
+
+        public virtual decimal CalcFrete()
+        {
+            return Convert.ToDecimal(distancia * peso) * Convert.ToDecimal(0.01);
+
+        }
+
+        public override string ToString()
+        {
+            return $"{distancia} --- {peso} --- {CalcFrete()}";
+        }
+    }
+}
---
+++ Lista19solution/Lista19solution/FreteExpresso.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista19solution
+{
+    class FreteExpresso : Frete
+    {
+        private decimal seguro;
+
+        public FreteExpresso(double p, double d, decimal seg) : base(p, d)
+        {
+            seguro = seg;
+        }
+
+        public override decimal CalcFrete()
+        {
+            return base.CalcFrete() * 2 + (decimal)(0.01) * seguro;
+        }
+        public override string ToString()
+        {
+            return $"FreteExpresso: {GetDistancia()} ----  {GetPeso()} {this.CalcFrete()}";
+        }
+     }
+
+ }
